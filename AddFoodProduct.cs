93bc83ce2b9b1febad224960a134c26b2fdaef73
Eyosias <eyosiasbin123@gmail.com>
2022-10-11 23:56:46 +0300

    Added addPrduct form
    form of add product in restaurant side

---
+++ AddFoodProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enibla_project
+{
+    public partial class AddFoodProduct : Form
+    {
+        public AddFoodProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblmilkproduct_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
