6e8d143ef1a8d67ff37dabac02a2823c1b0a974d
Eyosias <eyosiasbin123@gmail.com>
2022-10-19 18:08:53 +0300

    Delivery Login
    added login page

--- DeliveryLogin.cs
+++ DeliveryLogin.cs
@@ -16,5 +16,10 @@ namespace Enibla_project
         {
             InitializeComponent();
         }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
